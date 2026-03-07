c9718232f6aa3dc5eddb64c871c7641941fb1b07
= <=>
2026-03-07 18:36:38 -0300

    feat(enemy): add movement logic and map interaction
    - Introduced SYMBOL attribute for map representation
    - Added directions dictionary for movement vectors
    - Added isBlocked() method to check if enemy's position is blocked by walls or obstacles
    - Added move() method to select random movement and handle player collision

--- model/Enemy.py
+++ model/Enemy.py
@@ -2,20 +2,33 @@ import random
 
 class Enemy:
 
-    def __init__(self):
-        pass
+    SYMBOL = "E"
 
-    def isBlocked(self, position):
-        if (position == "+" or position == "#"):
+    directions = {
+        "w" : (-1,0),
+        "a" : (0,-1),
+        "s" : (1,0),
+        "d" : (0,1)
+        }
+
+    def __init__(self, position):
+        self.position = position
+
+    def isBlocked(self):
+        row, col = self.position
+        if self.gameMap[row][col] == "+" or self.gameMap[row][col] == "#":
             return True
         else:
             return False
 
-    def move(self, position):
-        if isBlocked(position):
+    def move(self):
+        pairs = list(self.directions.values())
+        row, col = random.choice(pairs)
+
+        if self.isBlocked(self.position):
             print("Can't move, position is blocked")
         
-        if (position == "P"):
+        if (self.position == "P"):
             print("Enemy killed Played. Game Over...")
         else:
             pass
