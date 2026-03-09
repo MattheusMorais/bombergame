efc958f5a496e93c9bded77b059dbece38260fde
= <=>
2026-03-09 00:14:47 -0300

    Refactor Gameplay initialization and integrate bomb & enemy logic
    - Moved GameState instantiation to __init__ of Gameplay for easier access
    - Added tracking of survivedTurns, roundsPlayed, bombsUtilized, and initial_number_of_enemies
    - Converted player and enemies to instance variables for consistent access
    - Updated map method names to snake_case: get_free_positions(), update_cell(), print_map()
    - Integrated bomb ticking and chain explosions after each player move
    - Moved enemy movement to follow each player action
    - Ensured map updates and prints after every move and bomb tick
    - Prepared structure for game turn loop with proper references to player and enemies

--- Main.py
+++ Main.py
@@ -1,10 +1,9 @@
 import os
-
 from controller.Gameplay import Gameplay
 
 def main():
-    gamePlay = Gameplay()
-    gamePlay.run()
+    game_play = Gameplay()
+    game_play.run()
 
 if __name__ == "__main__":
     os.system('cls' if os.name == 'nt' else 'clear') 
--- controller/Gameplay.py
+++ controller/Gameplay.py
@@ -1,5 +1,4 @@
 import random
-import os
 
 from model.Enemy import Enemy
 from model.Map import Map
@@ -11,41 +10,116 @@ from view.StartMenu import StartMenu
 class Gameplay:
 
     def __init__(self):
-        pass
+        self.game_state = GameState()
+        self.game_state.open()
+        self.survivedTurns = self.game_state.config["survivedTurns"]
+        self.roundsPlayed = self.game_state.config["roundsPlayed"]
+        self.bombsUtilized = self.game_state.config["bombsUtilized"]
+        self.initial_number_of_enemies = self.game_state.config["enemyStart"]
+        self.enemies = []
 
     def run(self):
-        # Creating GameState
-
-        gameState = GameState()
-        gameState.open()
-
+        
         # Start Menu
-        # start_menu = StartMenu(gameState)
+        start_menu = StartMenu(self.game_state)
 
         # Creating start Map
-        start_game_map = Map(gameState)
+        start_game_map = Map(self.game_state)
 
         # Getting free positions on map
-        free_positions = start_game_map.getFreePositions()  # return list of (row, col)
+        free_positions = start_game_map.get_free_positions()  # return list of (row, col)
 
         # Creating player
-        player1 = Player()
+        player_1 = Player(self.game_state)
 
         # Creating enemies and putting on map
-        enemies = []
-        initial_number_of_enemies = gameState.config["enemyStart"]
-
-        for enemy in range(initial_number_of_enemies):
+        
+        for enemy in range(self.initial_number_of_enemies):
             free_position = random.choice(free_positions)
             free_positions.remove(free_position)
             enemy = Enemy(free_position)
-            enemies.append(enemy)
+            self.enemies.append(enemy)
             row, col = free_position
-            start_game_map.updateCell(row, col, enemy.SYMBOL)
+            start_game_map.update_cell(row, col, enemy.SYMBOL)
 
         # Print map
-        start_game_map.printMap()
+        start_game_map.print_map()
 
         # Creating Obstacle
-        obstacle = Obstacles(gameState)
+        obstacle = Obstacles(self.game_state)
+
+        # Player move
+        player_1.move("d", start_game_map)
+        for bomb in player_1.active_bombs[:]:
+            if bomb.tick():
+                start_game_map.chain_explosion(bomb, player_1, self.enemies)
+                player_1.active_bombs.remove(bomb)
+        for enemy in self.enemies:
+            enemy.move(start_game_map)
+        start_game_map.print_map()
+
+        player_1.move("s", start_game_map)
+        for bomb in player_1.active_bombs[:]:
+            if bomb.tick():
+                start_game_map.chain_explosion(bomb, player_1, self.enemies)
+                player_1.active_bombs.remove(bomb)
+        for enemy in self.enemies:
+            enemy.move(start_game_map)
+        start_game_map.print_map()
+
+        player_1.put_bomb("f", start_game_map)
+        for bomb in player_1.active_bombs[:]:
+            if bomb.tick():
+                start_game_map.chain_explosion(bomb, player_1, self.enemies)
+                player_1.active_bombs.remove(bomb)
+        for enemy in self.enemies:
+            enemy.move(start_game_map)
+        start_game_map.print_map()
+
+        player_1.move("d", start_game_map)
+        for bomb in player_1.active_bombs[:]:
+            if bomb.tick():
+                start_game_map.chain_explosion(bomb, player_1, self.enemies)
+                player_1.active_bombs.remove(bomb)
+        for enemy in self.enemies:
+            enemy.move(start_game_map)
+        start_game_map.print_map()
+
+        player_1.move("s", start_game_map)
+        for bomb in player_1.active_bombs[:]:
+            if bomb.tick():
+                start_game_map.chain_explosion(bomb, player_1, self.enemies)
+                player_1.active_bombs.remove(bomb)
+        for enemy in self.enemies:
+            enemy.move(start_game_map)
+        start_game_map.print_map()
+
+        player_1.move("a", start_game_map)
+        for bomb in player_1.active_bombs[:]:
+            if bomb.tick():
+                start_game_map.chain_explosion(bomb, player_1, self.enemies)
+                player_1.active_bombs.remove(bomb)
+        for enemy in self.enemies:
+            enemy.move(start_game_map)
+        start_game_map.print_map()
+
+        player_1.move("a", start_game_map)
+        for bomb in player_1.active_bombs[:]:
+            if bomb.tick():
+                start_game_map.chain_explosion(bomb, player_1, self.enemies)
+                player_1.active_bombs.remove(bomb)
+        for enemy in self.enemies:
+            enemy.move(start_game_map)
+        start_game_map.print_map()
+
+        player_1.move("a", start_game_map)
+        for bomb in player_1.active_bombs[:]:
+            if bomb.tick():
+                start_game_map.chain_explosion(bomb, player_1, self.enemies)
+                player_1.active_bombs.remove(bomb)
+        for enemy in self.enemies:
+            enemy.move(start_game_map)
+        start_game_map.print_map()
+
+
 
