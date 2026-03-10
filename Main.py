46b1a3bfc9dc7e7911372c26ea6e9f1ad1418ebb
= <=>
2026-03-10 20:59:41 -0300

    feat(main): add game entrypoint with main loop
    - Implements main loop to run the game continuously and allow restart after save JSON when inputted "facil"
    - Calls Gameplay.game_loop() and exits if game signals closure
    - Clears screen at start using clear_screen()

--- Main.py
+++ Main.py
@@ -2,8 +2,12 @@ from model.Helper import clear_screen
 from controller.Gameplay import Gameplay
 
 def main():
-    game_play = Gameplay()
-    game_play.game_loop()
+    while True:
+        game_play = Gameplay()
+        close_game = game_play.game_loop()
+
+        if close_game:
+            return
 
 if __name__ == "__main__":
     clear_screen()
